990c68dd62f9de63732fb63bba4ba887fc7551a6
Callum Fairbairn <86301717+callum-fairbairn-cazoo@users.noreply.github.com>
2022-12-25 23:07:14 +0000

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
+import println
+import readInput
+import java.io.File
+
+fun customReadInput(name: String) = File("src", "$name.txt")
+    .readText().split("\n\n")
+
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.maxOf { group ->
+            group.split("\n").sumOf { Integer.parseInt(it) }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var window = intArrayOf(0, 0, 0)
+        input.forEach { group ->
+            val total = group.split("\n").sumOf { Integer.parseInt(it) }
+            if (total > window.min()) {
+                window[window.indexOf(window.min())] = total
+            }
+        }
+
+        return window.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = customReadInput("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = customReadInput("Day01")
+    check(part1(input) == 69177)
+    check(part2(input) == 207456)
 }
